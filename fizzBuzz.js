6d955378048e5e8d38537a31d07627ac08540018
Sankatouille <affile.benjamin@gmail.com>
2021-09-22 12:27:26 +0200

    test validé

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,5 +1,33 @@
-const fizzBuzz = () => {
+const fizzBuzz = (number) => {
   // Start here
-};
+
+let result;
+
+if (number % 3 == 0 && number % 5 == 0) {
+  
+  result = 'fizzbuzz'; 
+}
+ else if (number % 3 == 0){
+
+result = "fizz";
+
+ }
+
+else if (number % 5 == 0) {
+  
+  result = "buzz";
+
+ }
+else {
+  result = number;
+}
+return result;
+}
+
+
+
+
+
+
 
 module.exports = fizzBuzz;
